7475c3af55e6063262268ecc015318a4976c2135
btcardwell <bryan.cardwell@gmail.com>
2025-01-09 13:53:35 +0000

    debugging

--- sidm/__init__.py
+++ sidm/__init__.py
@@ -2,4 +2,4 @@ from dask.distributed import print
 import dask
 import os
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-print("__init__ time! BASE_DIR: {BASE_DIR}")
+print(f"__init__ time! BASE_DIR: {BASE_DIR}")
