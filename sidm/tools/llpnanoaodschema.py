8ba94788fc1b470bdcc749062799efbcaf1d1d82
btcardwell <bryan.cardwell@gmail.com>
2025-02-27 13:42:11 +0000

    In-progress llpnanoaodschema implementation and tests

--- sidm/tools/llpnanoaodschema.py
+++ sidm/tools/llpnanoaodschema.py
@@ -14,11 +14,36 @@ class LLPNanoAODSchema(NanoAODSchema):
     all_cross_references = {
         **NanoAODSchema.all_cross_references,
         "DSAMuon_muonMatch1idx": "Muon",
+        "DSAMuon_muonMatch2idx": "Muon",
+        "DSAMuon_muonMatch3idx": "Muon",
+        "DSAMuon_muonMatch4idx": "Muon",
+        "DSAMuon_muonMatch5idx": "Muon",
+    }
+    nested_items = {
+        **NanoAODSchema.nested_items,
+        "DSAMuon_muonIdxG": [
+            "DSAMuon_muonMatch1idxG",
+            "DSAMuon_muonMatch2idxG",
+            "DSAMuon_muonMatch3idxG",
+            "DSAMuon_muonMatch4idxG",
+            "DSAMuon_muonMatch5idxG",
+        ],
     }
 
     @property
     def behavior(cls):
         """Behaviors necessary to implement this schema (dict)"""
-        from coffea.nanoevents.methods import nanoaod
+        import awkward
+        from coffea.nanoevents.methods import nanoaod, base, candidate
+        
+        @awkward.mixin_class(nanoaod.behavior)
+        class DSAMuon(candidate.PtEtaPhiMCandidate, base.NanoCollection, base.Systematic):
+            """LLPNanoAOD DSA muon object"""
+ 
+            @property
+            def matched_muon(self):
+                return self._events().Muon._apply_global_index(self.muonIdxG)
+
+        nanoaod._set_repr_name("DSAMuon")
 
         return nanoaod.behavior
