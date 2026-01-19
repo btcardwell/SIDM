ffd54f3b597c6da8238f393d618ec3fe807da90a
mariajose2398 <mariajose2398@gmail.com>
2026-01-19 03:32:15 +0000

    added lost hits cuts for egm lj electrons

--- sidm/definitions/cuts.py
+++ sidm/definitions/cuts.py
@@ -47,6 +47,7 @@ obj_cut_defs = {
         "1gLj": lambda objs: (objs["egm_ljs"].electron_n == 0) & (objs["egm_ljs"].photon_n == 1),
         "2gLj": lambda objs: (objs["egm_ljs"].electron_n == 0) & (objs["egm_ljs"].photon_n == 2),
         "egm_lj_iso < 0.2": lambda objs: objs["egm_ljs"].isolation < 0.2,
+        "lostHits >= 1": lambda objs: ak.min(objs["egm_ljs"].electrons.trkNumPixelHits, axis=-1) >= 1,
     },
     "mu_ljs": {
         "pfMuLj": lambda objs: (objs["mu_ljs"].pfMu_n > 0) & (objs["mu_ljs"].dsaMu_n == 0),
